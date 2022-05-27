89ecb4748f8f46f4717e78f76063cec29b7519e1
Vasiliy Streltsov <streltcov.vasiliy.bpo1401@yandex.ru>
2022-05-27 23:30:00 +0300

    Forgotten file

--- SimpleTaskApi/Services/QuartzService.cs
+++ SimpleTaskApi/Services/QuartzService.cs
@@ -1,5 +1,6 @@
 using Quartz;
 using Quartz.Impl;
+using SimpleTaskApi.Domain;
 using SimpleTaskApi.Interfaces;
 using SimpleTaskApi.Jobs;
 
@@ -8,15 +9,16 @@ namespace SimpleTaskApi.Services;
 public class QuartzService : IQuartzService
 {
     private readonly IScheduler _scheduler;
-
-    public QuartzService(IServiceProvider serviceProvider)
+    
+    public QuartzService(IServiceProvider serviceProvider, ILogger<SetTaskStatusJobListener> listenerLogger)
     {
         var factory = new StdSchedulerFactory();
         _scheduler = factory.GetScheduler().Result;
         _scheduler.JobFactory = new SetTaskStatusJobFactory(serviceProvider);
+        _scheduler.ListenerManager.AddJobListener(new SetTaskStatusJobListener(listenerLogger));
     }
     
-    public async Task ScheduleTaskStatusChange(Guid taskId, int statusId)
+    public async Task ScheduleTaskStatusChange(Guid taskId)
     {
         IJobDetail startJob = JobBuilder.Create<SetTaskStatusJob>()
             .WithIdentity($"Setting status for task {taskId}")
@@ -24,7 +26,7 @@ public class QuartzService : IQuartzService
         ITrigger triggerStartJob = TriggerBuilder.Create()
             .WithIdentity($"Trigger setting status for task {taskId}")
             .UsingJobData("taskId", taskId)
-            .UsingJobData("statusId", statusId)
+            .UsingJobData("statusId", (int)Status.Running)
             .StartNow()
             .WithSimpleSchedule(x => x.Build())
             .Build();
